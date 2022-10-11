64c8a46496440325e5d6e18e123a7ac0a12c1cad
Abhaymithrachintaparthi <110761914+Abhaymithrachintaparthi@users.noreply.github.com>
2022-10-11 14:58:01 +0530

    Update question2.py

--- question2.py
+++ question2.py
@@ -1,5 +1,5 @@
 employee={"name":"tim","age":30,"birthday":"1990-03-10","job":"DevOps Engineer"}
 employee["job"] = "Software Engineer"
-employee.__delitem__("age")
+del employee["age"]
 for i,j in employee.items():
     print(i,':',j)
