053b1b40e2a39c99ab0da72ab2be815e031d3e14
Abhaymithrachintaparthi <110761914+Abhaymithrachintaparthi@users.noreply.github.com>
2023-01-27 12:19:50 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,14 +1,12 @@
 provider "aws" {
     region = "us-east-1"
-    access_key = "AKIAZJNUPWVQJ3EJQA4Q"
-    secret_key = "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"
+    access_key = "AKIAZJNUPWVQCKGU6ALZ"
+    secret_key = "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"
 }
 resource "aws_instance" "ubuntu" {
     ami = "ami-00874d747dde814fa"
     instance_type = "t2.micro"
-    subnet_id = "subnet-0df3ef6c4b95f87cd"
-    security_groups = [ "sg-01f685398241499a7" ]
     tags = {
-      "name" = "ec2_instance"
+      Name = "ubuntu"
     }
 }
